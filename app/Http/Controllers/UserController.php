3e3c70ee20cb4ec194e752acf68d28426b04e0bc
Your Name <you@example.com>
2021-10-07 04:04:48 -0700

    login middleware

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    function login(Request $req){
+        $user= User::where(['email'=>$req->email])->first();
+        if(!$user || Hash::check($req->pass,$user->password)){
+            return "user name or pass does not match";
+        }else{
+            $req->session()->put('user',$user);
+            return redirect('/home');
+        }
+    }
+}
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,16 +1,19 @@
 @extends('master')
 @section('content')
     <div class="container col-sm-4 custom-login">
-        <form>
-            <div class="mb-3">
+        <form action="/login" method="Post">
+           <div class="form-group">
+            @csrf
+             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <input type="password" name="pass" class="form-control" id="exampleInputPassword1">
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary">Login</button>
+           </div>
         </form>
     </div>
 @endsection
